104684b1b79e99743b632760878a34fbe3f2388a
bharath <kallepallibharath@gmail.com>
2019-07-06 21:50:48 +0530

    reseller

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,16 +1,23 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
+import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { LoginModule } from './login/login.module';
+import { ServicService } from './servic.service';
+import { SellerModule } from './seller/seller.module';
+
+
 
 @NgModule({
-  declarations: [
-    AppComponent
+  declarations: [AppComponent
+ 
   ],
   imports: [
-    BrowserModule
+    BrowserModule,LoginModule,
+    AppRoutingModule
   ],
-  providers: [],
+  providers: [ ServicService],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
